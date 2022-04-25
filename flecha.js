2400da4469b55273ca4306ab96fefd83038fb019
Angel Travieso <angeltraviesoc@gmail.com>
2022-04-25 17:54:08 -0400

    Reforzamiento js

---
+++ flecha.js
@@ -0,0 +1,15 @@
+// function sumar( a, b ) {
+//     return a + b;
+// }
+
+const sumar = ( a, b ) => a + b;
+
+const saludar = () => 'Hola Mundo';
+
+// function saludar() {
+//     return 'Hola Mundo';
+// }
+
+console.log( sumar( 5, 10 ) );
+console.log(saludar());
+
